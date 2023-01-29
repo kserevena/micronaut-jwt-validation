410f7afab5a8d8126ca22375a005cd6a0ee02726
Karl Serevena <karl.serevena@qumu.com>
2023-01-29 17:17:30 +0000

    Test that expired JWT is rejected by validation

--- src/test/java/com/qumu/ApiTest.java
+++ src/test/java/com/qumu/ApiTest.java
@@ -16,10 +16,13 @@ import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+import java.util.Date;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatException;
 
 @MicronautTest
 class ApiTest {
@@ -27,28 +30,14 @@ class ApiTest {
     @Value("${security.hs256.secret}")
     String jwtSigningSecret;
 
-    SignedJWT signedJWT;
     @Inject
     @Client("/")
     HttpClient client;
 
-    /**
-     * Build a valid signed JWT for use in tests using secret provided in config
-     */
-    @BeforeEach
-    void setup() throws JOSEException {
-        MACSigner signer = new MACSigner(jwtSigningSecret);
-        JWTClaimsSet claims = new JWTClaimsSet.Builder()
-                .subject("testUser")
-                .build();
-        signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claims);
-        signedJWT.sign(signer);
-    }
-
     @Test
-    void greetingGetEndpointRespondsToAuthorizedRequests() {
+    void greetingGetEndpointRespondsToAuthorizedRequests() throws JOSEException {
 
-        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.GET("/greeting").bearerAuth(signedJWT.serialize()), String.class);
+        HttpResponse<String> response = client.toBlocking().exchange(HttpRequest.GET("/greeting").bearerAuth(validJwt()), String.class);
         assertThat(response.code()).isEqualTo(HttpStatus.OK.getCode());
         assertThat(response.body()).isEqualTo("Hello World!");
     }
@@ -56,9 +45,54 @@ class ApiTest {
     @Test
     void greetingGetEndpointRejectsUnauthorizedRequests() {
 
-        Assertions.assertThatException().isThrownBy(() -> client.toBlocking().exchange(HttpRequest.GET("/greeting").bearerAuth("a bad token"), String.class)).isInstanceOf(HttpClientResponseException.class).satisfies(e -> {
+        assertThatException().isThrownBy(() -> client.toBlocking().exchange(HttpRequest.GET("/greeting").bearerAuth("a bad token"), String.class)).isInstanceOf(HttpClientResponseException.class).satisfies(e -> {
             HttpClientResponseException castException = (HttpClientResponseException) e;
             Assertions.assertThat((CharSequence) castException.getStatus()).isEqualTo(HttpStatus.UNAUTHORIZED);
         });
+    }
+
+    @Test
+    void greetingGetEndpointRejectsExpiredJwts() {
+
+        assertThatException().isThrownBy(() -> client.toBlocking().exchange(HttpRequest.GET("/greeting").bearerAuth(expiredJwt()), String.class)).isInstanceOf(HttpClientResponseException.class).satisfies(e -> {
+            HttpClientResponseException castException = (HttpClientResponseException) e;
+            Assertions.assertThat((CharSequence) castException.getStatus()).isEqualTo(HttpStatus.UNAUTHORIZED);
+        });
+
+    }
+
+    /**
+     * Build a valid signed JWT for use in tests using secret provided in config
+     *
+     * @return valid signed JWT
+     */
+    private String validJwt() throws JOSEException {
+        return buildSignedJwt(false).serialize();
+    }
+
+    /**
+     * @return An expired JWT with valid signature
+     */
+    private String expiredJwt() throws JOSEException {
+        return buildSignedJwt(true).serialize();
+    }
+
+    private SignedJWT buildSignedJwt(boolean expired) throws JOSEException {
+
+        Instant now = Instant.now();
+        if (expired) {
+            now = now.minusSeconds(30);
+        } else {
+            now = now.plusSeconds(1000);
+        }
+
+        MACSigner signer = new MACSigner(jwtSigningSecret);
+        JWTClaimsSet claims = new JWTClaimsSet.Builder()
+                .subject("testUser")
+                .expirationTime(Date.from(now))
+                .build();
+        SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claims);
+        signedJWT.sign(signer);
+        return signedJWT;
     }
 }
